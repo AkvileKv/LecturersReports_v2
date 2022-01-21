33ab693e3175f8f33fab4d777284b5dee0b78b9d
AkvileKv <kvietkauskaite.akvile@gmail.com>
2022-01-21 14:49:19 +0200

    Finish user schema

--- models/user.js
+++ models/user.js
@@ -141,7 +141,7 @@ var tMTEP3_T09Schema = new mongoose.Schema({
 });
 var tMTEP3_T10Schema = new mongoose.Schema({
   nr: String,
-  atlikejas: String,
+  destytojas: String,
   veiklPobud: String,
   veiklTiksl: String,
   dataVieta: String,
@@ -186,7 +186,8 @@ var kompetencijuSchema = new mongoose.Schema({
   pavadinimas: String,
   pazymNr: String,
   trukmeValLT: Number,
-  trukmeValUžs: Number
+  trukmeValNeLT: Number,
+  destytojas: String
 });
 var kTOV4_25Schema = new mongoose.Schema({
   nr: String,
@@ -201,9 +202,9 @@ var kTOV4_26Schema = new mongoose.Schema({
   trukmeVal: Number,
   data: String
 });
-
 var kTOV4_KV03Schema = new mongoose.Schema({
   nr: String,
+  destytojas: String,
   studKryptis: String,
   salis: String,
   institucija: String,
@@ -211,6 +212,7 @@ var kTOV4_KV03Schema = new mongoose.Schema({
 });
 var kTOV4_O01_1Schema = new mongoose.Schema({
   nr: String,
+  destytojas: String,
   veiklPobud: String,
   isakNrData: String
 });
@@ -247,7 +249,8 @@ var kV5_KT02Schema = new mongoose.Schema({
   darboTema: String
 });
 var kV5_32Schema = new mongoose.Schema({
-  aprasymas: String
+  aprasymas: String,
+  destytojas: String
 });
 var kV5_33Schema = new mongoose.Schema({
   nr: String,
@@ -268,7 +271,6 @@ var kV5_34Schema = new mongoose.Schema({
   rezultatai: String,
   salisData: String
 });
-
 //katedros vedeju schemos
 var kKPP1_1Schema = new mongoose.Schema({
   nr: String,
@@ -384,15 +386,14 @@ var tMTEP3_kT05Schema = new mongoose.Schema({
 var tMTEP3_162Schema = new mongoose.Schema({
   nr: String,
   destytojas: String,
-  veiklPavad: String,
-  veiklRezult: String,
-  dataVieta: String
+  pavadinimas: String,
+  pastabos: String
 });
 var tMTEP3_kT16Schema = new mongoose.Schema({
   nr: String,
+  rengejai: String,
   pavadinimas: String,
-  uzsakovas: String,
-  rengejai: String
+  uzsakovas: String
 });
 var tMTEP3_kT12Schema = new mongoose.Schema({
   nr: String,
@@ -415,8 +416,77 @@ var tMTEP3_kT14Schema = new mongoose.Schema({
   veiklPobud: String,
   dataVieta: String
 });
-
-
+var kTOV4_27Schema = new mongoose.Schema({
+  nr: String,
+  renginysTema: String,
+  kompGrupe: String,
+  skirta: String,
+  lektorius: String,
+  lektTipas: String
+});
+var kTOV4_28Schema = new mongoose.Schema({
+  nr: String,
+  destytojas: String,
+  imonIstaig: String,
+  kompGrupe: String,
+  trukmeVal: Number,
+  data: String
+});
+var kTOV4_30Schema = new mongoose.Schema({
+  nr: String,
+  destytojas: String,
+  studKryptis: String,
+  salis: String,
+  institucija: String,
+  dalykas: String
+});
+var kV5_ksp32Schema = new mongoose.Schema({
+  nr: String,
+  studKryptis: String,
+  studProgr: String,
+  strategPartner: String
+});
+var kV5_bcp33Schema = new mongoose.Schema({
+  nr: String,
+  veikla: String,
+  socPartneris: String,
+  destytojas: String
+});
+var kV5_sabp34Schema = new mongoose.Schema({
+  nr: String,
+  studKryptis: String,
+  studProgr: String,
+  studentuSk1: Number,
+  studentuSk2: Number,
+  studentuSk3: Number,
+  studentuSk4: Number,
+  studentuSk5: Number
+});
+var kV5_35Schema = new mongoose.Schema({
+  nr: String,
+  studKryptis: String,
+  studProgr: String,
+  studentuSk1: Number,
+  pasPartnerSk1: Number,
+  studentuSk2: Number,
+  pasPartnerSk2: Number,
+  studentuSk3: Number,
+  pasPartnerSk3: Number,
+  studentuSk4: Number,
+  pasPartnerSk4: Number,
+  studentuSk5: Number,
+  pasPartnerSk5: Number
+});
+var kV5_39Schema = new mongoose.Schema({
+  nr: String,
+  kryptys: String,
+  aprasymas: String
+});
+var veiklSavinaliz_Schema = new mongoose.Schema({
+  nr: String,
+  stiprybes: String,
+  tobulintina: String
+});
 
 
 var userSchema = new mongoose.Schema({ //pagrindinė schema
@@ -433,10 +503,10 @@ var userSchema = new mongoose.Schema({ //pagrindinė schema
   updated_for: String,
 
   destytojas: {
-    pareigos: String,
+    pareigos: String, //1 lentelė
     darbovietesTipas: String,
     pedagogStazas: Number,
-    praktVeiklStazas: Number, //1 lentelė
+    praktVeiklStazas: Number,
     kD1_K01: { //2 lentelė
       kD1_K01_array: [kD1_K01Schema],
       isVisoValPlan: Number,
@@ -518,7 +588,7 @@ var userSchema = new mongoose.Schema({ //pagrindinė schema
         bendrosios: [kompetencijuSchema],
         dalykines: [kompetencijuSchema],
         isVisoValLT: Number,
-        isVisoValUžs: Number,
+        isVisoValNeLT: Number,
         dalyvavoDalyk: Boolean
       }
     },
@@ -552,186 +622,124 @@ var userSchema = new mongoose.Schema({ //pagrindinė schema
       kitosVeikl: String
     },
     ataskaitosPateikimoData: String,
-  }
-  // //, // !!! ok viskas iki vedejo
-  //
-  // katedrosVedejas: {
-  //   kKPP1_1: [kKPP1_1Schema], //1 lentelė
-  //   kDS1: [kDS1Schema], // 2
-  //   kKPP1_3: [kKPP1_3Schema], //3
-  //   mV2: {
-  //     mV2_M02: [mV2_M02Schema], //4
-  //     mV2_M03: [mV2_M03Schema], //10
-  //     mV2_M04: [mV2_M04Schema], //5
-  //     mV2_D01: [mV2_D01Schema], //7
-  //     mV2_D02: [mV2_D02Schema], //8
-  //     mV2_D03: [mV2_D03Schema], //9
-  //     mV2_D06: [mV2_D06Schema], //6
-  //     mV2_S01: [mV2_S01Schema], //11
-  //     mV2_S: [mV2_SSchema]
-  //   },
-  //   tMTEP3: {
-  //     tMTEP3_T01: [tMTEP3_kT01Schema], //12 lentelė
-  //     tMTEP3_T02: [tMTEP3_T02Schema], //13
-  //     tMTEP3_T03: [tMTEP3_T03Schema], //14
-  //     tMTEP3_T04: [tMTEP3_kT04Schema], //15
-  //     tMTEP3_T05: [tMTEP3_kT05Schema], //16.1
-  //     tMTEP3_162: [tMTEP3_162Schema], //16.2
-  //     tMTEP3_T06: [tMTEP3_T06Schema], //17
-  //     tMTEP3_T07: [tMTEP3_T07Schema], //18
-  //     tMTEP3_T08: [tMTEP3_T08Schema], // 19
-  //     tMTEP3_T09: [tMTEP3_T09Schema], //20
-  //     tMTEP3_T10: [tMTEP3_T10Schema], //21
-  //     tMTEP3_T11: [tMTEP3_T11Schema], //22
-  //     tMTEP3_T12: [tMTEP3_kT12Schema], //23
-  //     tMTEP3_T13: [tMTEP3_kT13Schema],//25
-  //     tMTEP3_T14: [tMTEP3_kT14Schema], //24
-  //     tMTEP3_T16: [tMTEP3_kT16Schema], //16.3
-  //     tMTEP3_S: [tMTEP3_SSchema]
-  //   }, //testii toliau
-  //   kTOV4: {
-  //     kTOV4_KV01: {
-  //       dalykinesLT: {
-  //         destytojuSk: Number,
-  //         trukmeVal: Number,
-  //         stazuotSk: Number,
-  //         seminarSk: Number,
-  //         konferencSk: Number,
-  //         kursaiSk: Number
-  //       },
-  //       didaktinesLT: {
-  //         destytojuSk: Number,
-  //         trukmeVal: Number,
-  //         stazuotSk: Number,
-  //         seminarSk: Number,
-  //         konferencSk: Number,
-  //         kursaiSk: Number
-  //       },
-  //       bendrosiosLT: {
-  //         destytojuSk: Number,
-  //         trukmeVal: Number,
-  //         stazuotSk: Number,
-  //         seminarSk: Number,
-  //         konferencSk: Number,
-  //         kursaiSk: Number
-  //       },
-  //       kTOV4_KV01dalykines_komp: {
-  //         stazuotes: [stazuotes_kSchema],
-  //         seminarai: [seminarai_kSchema],
-  //         konferencijos: [konferencijos_kSchema],
-  //         kursai: [kursai_kSchema]
-  //       },
-  //       kTOV4_KV01didaktines_komp: {
-  //         stazuotes: [stazuotes_kSchema],
-  //         seminarai: [seminarai_kSchema],
-  //         konferencijos: [konferencijos_kSchema],
-  //         kursai: [kursai_kSchema]
-  //       },
-  //       kTOV4_KV01bendrosios_komp: {
-  //         stazuotes: [stazuotes_kSchema],
-  //         seminarai: [seminarai_kSchema],
-  //         konferencijos: [konferencijos_kSchema],
-  //         kursai: [kursai_kSchema]
-  //       }
-  //     },
-  //     kTOV4_KV02: {
-  //       dalykinesNeLt: {
-  //         destytojuSk: Number,
-  //         trukmeVal: Number,
-  //         stazuotSk: Number,
-  //         seminarSk: Number,
-  //         konferencSk: Number,
-  //         kursaiSk: Number
-  //       },
-  //       didaktinesNeLt: {
-  //         destytojuSk: Number,
-  //         trukmeVal: Number,
-  //         stazuotSk: Number,
-  //         seminarSk: Number,
-  //         konferencSk: Number,
-  //         kursaiSk: Number
-  //       },
-  //       bendrosiosNeLt: {
-  //         destytojuSk: Number,
-  //         trukmeVal: Number,
-  //         stazuotSk: Number,
-  //         seminarSk: Number,
-  //         konferencSk: Number,
-  //         kursuoseSk: Number
-  //       },
-  //       kTOV4_KV02dalykines_komp: {
-  //         stazuotes: [stazuotes_kSchema],
-  //         seminarai: [seminarai_kSchema],
-  //         konferencijos: [konferencijos_kSchema],
-  //         kursai: [kursai_kSchema]
-  //       },
-  //       kTOV4_KV02didaktines_komp: {
-  //         stazuotes: [stazuotes_kSchema],
-  //         seminarai: [seminarai_kSchema],
-  //         konferencijos: [konferencijos_kSchema],
-  //         kursai: [kursai_kSchema]
-  //       },
-  //       kTOV4_KV02bendrosios_komp: {
-  //         stazuotes: [stazuotes_kSchema],
-  //         seminarai: [seminarai_kSchema],
-  //         konferencijos: [konferencijos_kSchema],
-  //         kursai: [kursai_kSchema]
-  //       }
-  //     },
-  //     kTOV4_KV03: [kTOV4_kKV03Schema],
-  //     kTOV4_KV04: {
-  //       judrumoDinamika: String
-  //     },
-  //     kTOV4_KV05: {
-  //       poveikisVeiklai: String
-  //     },
-  //     kTOV4_O01: {
-  //       kTOV4_O01_1: [kTOV4_kO01_1Schema],
-  //       kTOV4_O01_2: [kTOV4_kO01_2Schema]
-  //     },
-  //     kTOV4_O02: [kTOV4_O02_Schema],
-  //     kTOV4_S: [kTOV4_SSchema],
-  //   },
-  //   kV5: {
-  //     kV5_KT01: [kV5_KT01Schema],
-  //     kV5_KT02: [kV5_KT02Schema],
-  //     kV5_KT03: {
-  //       kV5_KT03_array: [kV5_kKT03Schema],
-  //       isVisoBaigSk: Number,
-  //       isVisoIregUzimt: Number,
-  //       isVisoIsidarbProc: Number
-  //     },
-  //     kV5_KT04: [kV5_kKT04Schema],
-  //     kV5_KT05: {
-  //       bendradarbiavSklaid: String,
-  //       praktVeiklSklaid: String,
-  //       dalyvavSklaid: String,
-  //     },
-  //     kV5_KT06: {
-  //       moklsPopSklaid: String,
-  //       tyrimuSklaid: String,
-  //       modernKultSklaid: String,
-  //       bendrLavinSklaid: String,
-  //       socAtskirtSklaid: String,
-  //       aplinkApsaugSklaid: String
-  //     },
-  //     kV5_KT07: {
-  //       kompUgdymas: String
-  //     },
-  //     kV5_KT08: {
-  //       rezFormulavimas: String
-  //     },
-  //     kV5_KT09: {
-  //       darnVystItrauk: String
-  //     },
-  //     veiklSavinalize: {
-  //       veiklSavinaliz_array: [veiklSavinaliz_Schema],
-  //       isvadosApieVeikl: String
-  //     }
-  //   }
-  // }
-});
+  },
 
+  katedrosVedejas: {
+    kKPP1_1: [kKPP1_1Schema], //1 lentelė
+    kDS1: [kDS1Schema], // 2
+    kKPP1_3: [kKPP1_3Schema], //3
+    mV2: {
+      mV2_M02: [mV2_M02Schema], //4
+      mV2_M03: [mV2_M03Schema], //10
+      mV2_M04: [mV2_M04Schema], //5
+      mV2_D01: [mV2_D01Schema], //7
+      mV2_D02: [mV2_D02Schema], //8
+      mV2_D03: [mV2_D03Schema], //9
+      mV2_D06: [mV2_D06Schema], //6
+      mV2_S01: [mV2_S01Schema], //11
+      mV2_S: [mV2_SSchema]
+    },
+    tMTEP3: {
+      tMTEP3_T01: [tMTEP3_kT01Schema], //12 lentelė
+      tMTEP3_T02: [tMTEP3_T02Schema], //13
+      tMTEP3_T03: [tMTEP3_T03Schema], //14
+      tMTEP3_T04: [tMTEP3_kT04Schema], //15
+      tMTEP3_T05: [tMTEP3_kT05Schema], //16.1
+      tMTEP3_162: [tMTEP3_162Schema], //16.2
+      tMTEP3_T06: [tMTEP3_T06Schema], //17
+      tMTEP3_T07: [tMTEP3_T07Schema], //18
+      tMTEP3_T08: [tMTEP3_T08Schema], // 19
+      tMTEP3_T09: [tMTEP3_T09Schema], //20
+      tMTEP3_T10: [tMTEP3_T10Schema], //21
+      tMTEP3_T11: [tMTEP3_T11Schema], //22
+      tMTEP3_T12: [tMTEP3_kT12Schema], //23
+      tMTEP3_T13: [tMTEP3_kT13Schema], //25
+      tMTEP3_T14: [tMTEP3_kT14Schema], //24
+      tMTEP3_T16: [tMTEP3_kT16Schema], //16.3
+      tMTEP3_S: [tMTEP3_SSchema]
+    },
+    kTOV4: {
+      kTOV4_KV01: {
+        kTOV4_26: { // 26 lentelė
+          mokymosi: {
+            destytojuSk: Number,
+            trukmeValLT: Number,
+            trukmeValNeLT: Number,
+            isVisoVal: Number
+          },
+          tyrimu: {
+            destytojuSk: Number,
+            trukmeValLT: Number,
+            trukmeValNeLT: Number,
+            isVisoVal: Number
+          },
+          bendrosios: {
+            destytojuSk: Number,
+            trukmeValLT: Number,
+            trukmeValNeLT: Number,
+            isVisoVal: Number
+          },
+          dalykines: {
+            destytojuSk: Number,
+            trukmeValLT: Number,
+            trukmeValNeLT: Number,
+            isVisoVal: Number
+          },
+          isVisoValLT: Number,
+          isVisoValNeLT: Number,
+          isVisoValBendr: Number
+        },
+        kompetencijos: { //26 lentelės priedas
+          mokymosi: [kompetencijuSchema],
+          tyrimu: [kompetencijuSchema],
+          bendrosios: [kompetencijuSchema],
+          dalykines: [kompetencijuSchema],
+          mokymosiIsVisoValLT: Number,
+          mokymosiIsVisoValNeLT: Number,
+          tyrimuIsVisoValLT: Number,
+          tyrimuIsVisoValNeLT: Number,
+          bendrosiosIsVisoValLT: Number,
+          bendrosiosIsVisoValNeLT: Number,
+          dalykinesIsVisoValLT: Number,
+          dalykinesIsVisoValNeLT: Number,
+          isVisoValLT: Number,
+          isVisoValNeLT: Number
+        }
+      },
+      kTOV4_27: [kTOV4_27Schema], //27 lentelė
+      kTOV4_28: [kTOV4_28Schema], //28 lentelė
+      kTOV4_KV03: [kTOV4_KV03Schema], //29 lentelė
+      kTOV4_30: [kTOV4_30Schema], //30 lentelė
+      kTOV4_O01: {
+        kTOV4_O01_1: [kTOV4_O01_1Schema], //31.1
+        kTOV4_O01_2: [kTOV4_O01_2Schema] //31.2
+      },
+      kTOV4_S: [kTOV4_SSchema],
+    },
+    kV5: {
+      kV5_32: [kV5_ksp32Schema], //32
+      kV5_33: [kV5_bcp33Schema], //33
+      kV5_34: [kV5_sabp34Schema], //34
+      kV5_35: [kV5_35Schema], //35
+      kV5_KT01: [kV5_KT01Schema], //37
+      kV5_KT02: [kV5_KT02Schema], //36
+      kV5_38: [kV5_34Schema], //38
+      kV5_39: [kV5_39Schema], //39
+      kV5_40: { //40
+        socAtskMaz: [kV5_32Schema],
+        aplinkosaugInic: [kV5_32Schema],
+        lietValstybPuosel: [kV5_32Schema],
+        lietEtnokPuos: [kV5_32Schema],
+        savanorystIniciatyv: [kV5_32Schema]
+      },
+      kV5_41: [kV5_33Schema], //41 lentelė
+      veiklSavinalize: {
+        veiklSavinaliz_array: [veiklSavinaliz_Schema],
+        isvadosApieVeikl: String
+      }
+    }
+  }
+});
 
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(mongooseHistory);
