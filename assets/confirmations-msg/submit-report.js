3a086dfe2aee24202ec993a5adbf37154f4e697f
AkvileKv <kvietkauskaite.akvile@gmail.com>
2022-11-18 16:02:29 +0200

    Add confirm submit report

--- assets/confirmations-msg/submit-report.js
+++ assets/confirmations-msg/submit-report.js
@@ -1,5 +1,18 @@
-document.getElementById("submitRep").addEventListener('click', submitMsgSave);
-//Pateikti
-function submitMsgSave() {
-    return confirm('Ar tikrai norite pateikti ataskaitą? Ataskaita bus užrakinta, daugiau nebegalėsite jos redaguoti');
-}
+window.addEventListener("DOMContentLoaded", function () {
+
+    document.getElementById("submitReportId").addEventListener('click', submitMsgSave);
+    var form = document.getElementById("form-submitReport-id");
+    
+    //Pateikti
+    function submitMsgSave() {
+        if (confirm('Ar tikrai norite pateikti ataskaitą? Ataskaita bus užrakinta, daugiau nebegalėsite jos redaguoti')) {
+            // Save it!
+            console.log('Taip');
+            form.submit();
+        } else {
+            // Do nothing!
+            console.log('Ne');
+            return false;
+        }
+    }
+});
